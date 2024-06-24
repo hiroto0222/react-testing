2f50c1e0531457a90d6f994659a9cec6db01e326
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-06-24 19:00:00 +0900

    feat: grouping tests using describe

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,21 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-/**
- * Greet should render the text "Hello"
- */
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textEle = screen.getByText("Hello");
-  expect(textEle).toBeInTheDocument();
-});
+describe("Greet", () => {
+  /**
+   * Greet should render the text "Hello"
+   */
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textEle = screen.getByText("Hello");
+    expect(textEle).toBeInTheDocument();
+  });
 
-/**
- * If a name is passed into the Greet component, it should render "Hello" followed by the passed name
- */
-test("Greet renders with a name", () => {
-  const name = "Hiroto";
-  render(<Greet name={name} />);
-  const textEle = screen.getByText("Hello " + name);
-  expect(textEle).toBeInTheDocument();
+  /**
+   * If a name is passed into the Greet component, it should render "Hello" followed by the passed name
+   */
+  test("renders with a name", () => {
+    const name = "Hiroto";
+    render(<Greet name={name} />);
+    const textEle = screen.getByText("Hello " + name);
+    expect(textEle).toBeInTheDocument();
+  });
 });
