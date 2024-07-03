4f5170e02f5f7bd9263a612025d16f553ad4c342
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-07-03 18:40:30 +0900

    act

--- README.md
+++ README.md
@@ -291,5 +291,19 @@ describe("useCounter", () => {
     });
     expect(result.current.count).toBe(10);
   });
+
+  // act() to make sure all updates related to "units" of interaction
+  // have been processed and applied to the dom before making any assertions.
+  it("increments the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+
+  it("decrements the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
+  });
 });
 ```
--- src/hooks/use-counter/useCounter.test.tsx
+++ src/hooks/use-counter/useCounter.test.tsx
@@ -1,4 +1,5 @@
 import { renderHook } from "@testing-library/react";
+import { act } from "react";
 import { useCounter } from "./useCounter";
 
 describe("useCounter", () => {
@@ -14,5 +15,19 @@ describe("useCounter", () => {
       },
     });
     expect(result.current.count).toBe(10);
+  });
+
+  // act() to make sure all updates related to "units" of interaction
+  // have been processed and applied to the dom before making any assertions.
+  it("increments the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+
+  it("decrements the count", () => {
+    const { result } = renderHook(useCounter);
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
   });
 });
